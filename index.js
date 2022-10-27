b0fde95ddc511b6526b979c9e71e3ddb65ddbf67
Miguel Curbelo <mcurbeloj@gmail.com>
2022-10-27 21:28:42 +0200

    finished the normal exercise whcih I didn't finish before

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+  this.items = [];
+  this.length = this.items.length;
+}
 
-  add(item) {}
+  add(item) {
+    if(item !== undefined){
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a,b)=> a-b);
+    }
+   return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length){
+      throw new Error ('OutOfBounds');
+    }
+    else {
+      return this.items[pos];
+    }
+    
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0){
+      return this.items[this.length - 1];
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0){
+      return this.items[0];
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length > 0){
+      this.summation = 0;
+      for (i=0; i<this.length;i++){
+        this.summation += this.items[i];
+      }
+      return this.summation;
+    }
+    else {
+      return 0;
+    }
+  }
 
-  avg() {}
+//   avg() {}
 }
 
 module.exports = SortedList;
